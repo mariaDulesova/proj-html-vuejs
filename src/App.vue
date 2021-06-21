05120e56c08a4d808eaf09b8f585eb6e512e5805
mariaDulesova <maria.dulesova@gmail.com>
2021-06-21 11:56:01 +0200

    Folder and data structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,122 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Header -->
+
+    <!-- /Header -->
+
+    <!-- Main -->
+    <main>
+      
+    </main>
+    <!-- /Main -->
+
+    <!-- Footer -->
+
+    <!-- /Footer -->
+  
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return{
+      menuMain: [
+        {
+          title: 'Home',
+          url: ''
+        },
+        {
+          title: 'Services',
+          url: ''
+        },
+        {
+          title: 'About',
+          url: ''
+        },
+        {
+          title: 'Videos',
+          url: ''
+        },
+        {
+          title: 'Blog',
+          url: ''
+        },
+        {
+          title: 'Store',
+          url: ''
+        }
+      ],
+      videos: [
+        {
+          url: './assets/video/video2-2x.jpg',
+          title: 'Thighs & glute workout',
+          subTitle: 'Increase your mobility'
+        },
+        {
+          url: './assets/video/video7-2x.jpg',
+          title: 'Lift, firm & perk up',
+          subTitle: 'Feel young again'
+        },
+        {
+          url: './assets/video/video9-2x.jpg',
+          title: 'Slim & trim your waist',
+          subTitle: 'Shed those extra pounds'
+        }
+
+      ],
+      articles:[
+        {
+          url:'./assets/img/blog4-2x-400x258.jpg',
+          title: 'The best protein shake',
+          post: 'By admin | November 26th, 2019 | Gym'
+        },
+        {
+          url:'./assets/img/blog1-2x-400x258.jpg',
+          title: 'Ultimate cardio workout',
+          post: 'By admin | November 26th, 2019 | Gym'
+        },
+        {
+          url:'./assets/img/blog3-2x-400x258.jpg',
+          title: 'New juices available now',
+          post: 'By admin | November 26th, 2019 | Gym'
+        },
+
+      ],
+      recentPosts: [
+        {
+          title: 'The best protein shake',
+          url:''
+        },
+        {
+          title: 'Ultimate cardio workout',
+          url:''
+        },
+        {
+          title: 'New juices available now',
+          url:''
+        },
+        {
+          title: 'Tips to find training partners',
+          url:''
+        },
+        {
+          title: '20 best healthy recipes',
+          url:''
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './style/general.scss'
 </style>
